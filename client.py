442f6c8d36422d2f033e25753d62bb197d339f32
IrinaMilekhina <GitNewlife2020>
2021-08-20 13:27:23 +0300

    test version

--- client.py
+++ client.py
@@ -1,53 +1,39 @@
-from socket import socket, AF_INET, SOCK_STREAM
-import random
 import asyncio
+import random
+import yaml
+
+with open('conf.yml') as f:
+    conf_data = yaml.safe_load(f)
+ports = list(conf_data.values())
 
 
-async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 8888)
+async def receive(reader, times):
+    for i in range(times):
+        data = await reader.read(100)
+        print(data.decode())
 
-    print(f'Send: {message!r}')
-    writer.write(message.encode())
-    await writer.drain()
 
-    data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
+async def send(writer, times, port):
+    for i in range(times):
+        interval = random.randint(1, 10)
+        s = str(random.randint(1, 10))
+        print(f'Sent: {s} to {port}')
+        writer.write(s.encode())
+        await asyncio.sleep(interval)
 
-    print('Close the connection')
+
+async def client(port):
+    reader, writer = await asyncio.open_connection('127.0.0.1', port)
+
+    await asyncio.gather(
+        asyncio.create_task(receive(reader, 10)),
+        asyncio.create_task(send(writer, 10, port))
+    )
     writer.close()
-    await writer.wait_closed()
-
-while True:
-    n = str(random.randint(0, 10))
-    asyncio.run(tcp_echo_client(n))
 
 
-# while True:
-#     s = socket(AF_INET, SOCK_STREAM)
-#     s.connect(('localhost', 8888))
-#     msg = str(random.randint(0, 10))
-#     s.send(msg.encode('utf-8'))
-#     print(f'отправлено на сервер: {msg}')
-#     data = s.recv(4096)
-#     print(data.decode('utf-8'))
-#     s.close()
+async def gen_connections():
+    await asyncio.gather(*[asyncio.create_task(client(port)) for port in ports])
 
 
-# async def sending():
-#     await asyncio.sleep(random.randint(0, 10))
-#     msg = str(random.randint(0, 10))
-#     s.send(msg.encode('utf-8'))
-#     print(f'отправлено на сервер: {msg}')
-#
-#
-# while True:
-#     ioloop = asyncio.get_event_loop()
-#     tasks = [ioloop.create_task(sending())]
-#     wait_tasks = asyncio.wait(tasks)
-#     ioloop.run_until_complete(wait_tasks)
-#
-#     ioloop.close()
-#     data = s.recv(4096)
-#     print(data.decode('utf-8'))
-#     s.close()
+asyncio.run(gen_connections())
